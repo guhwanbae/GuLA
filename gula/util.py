5e32ecee387bc30c128a947708e7be597079a2ee
Gu-hwan Bae <guhwan.bae@gmail.com>
2019-01-28 21:34:19 +0900

    Dot product between matrices and vectors.
      Adding a dot product for matrices or vectors.
      dot(vector, vector) is inner product between vectors.
      dot(matrix, vector) is multiplication between matrix and vector.
      Specially, Multiplication between matrix and vector is represented by
      linear combination of column vectors of matrix that scalar multiplied
      by coressponding element of vector, v[c].

--- gula/util.py
+++ gula/util.py
@@ -2,5 +2,28 @@
 # Date    : Sun Jan 28
 # Summary : Utilities and operators for matrix and vector.
 
+import gula.vec as gvec
+import gula.mat as gmat
+
 def cartesianProduct(set_a, set_b):
     return [(a, b) for a in set_a for b in set_b]
+
+def dot(u, v):
+    """
+    Return a dot product output between matrices and vectors.
+    Specially, Multiplication between matrix and vector is represented by
+    linear combination of column vectors of matrix that scalar multiplied
+    by coressponding element of vector, v[c].
+    """
+    if isinstance(u, gvec.vector) and isinstance(v, gvec.vector):
+        print('vec-vec')
+        if u.domain is v.domain:
+            return sum([u.getItem(d) * v.getItem(d) for d in u.domain])
+    elif isinstance(u, gmat.matrix) and isinstance(v, gvec.vector):
+        (row, col) = u.domain
+        if col is v.domain:
+            output = gvec.vector(row, {})
+            for c in col:
+                output = output + u.col(c) * v.getItem(c)
+            return output
+    return None
--- matrix/matrix-examples.py
+++ matrix/matrix-examples.py
@@ -3,12 +3,14 @@
 # Summary : Test a matrix modeling class and its utilities.
 
 import gula.mat as gmat
+import gula.vec as gvec
+import gula.util as gutil
 
 M = gmat.matrix(({'a','b'},{'@','#'}),
-               {('a','@'):1,
-                ('a','#'):2,
-                ('b','@'):3,
-                ('b','#'):4})
+                {('a','@'):1,
+                 ('a','#'):2,
+                 ('b','@'):3,
+                 ('b','#'):4})
 
 print('Matrix, M, 2x2')
 print(M)
@@ -25,3 +27,23 @@ for r in row:
 for c in col:
     print('Column vector, c =', c)
     print(M.col(c))
+
+A = gmat.matrix(({'x', 'y'}, {'a', 'b', 'c'}),
+                {('x','a'):1.,
+                 ('x','b'):2.,
+                 ('x','c'):3.,
+                 ('y','a'):4.,
+                 ('y','b'):5.,
+                 ('y','c'):6.})
+print('2x3 Matrix, A')
+print(A)
+
+(row, col) = A.domain
+x = gvec.vector(col, {'a':.1, 'b':.2, 'c':.3})
+print('R-3 Vector, x')
+print(x)
+
+b = gutil.dot(A, x)
+print('Multiplying matrix and vector, Ax = b')
+print('Output vector, b')
+print(b)
