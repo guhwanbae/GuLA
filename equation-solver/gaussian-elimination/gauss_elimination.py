558856a39f41c827ec19cdc93f209ea4a0e53acc
Gu-hwan Bae <guhwan.bae@gmail.com>
2019-02-23 21:29:22 +0900

    Get row echelon form matrix and find its basis.
      Get row echelon form matrix and find its basis by using 'gula.solve'.

---
+++ equation-solver/gaussian-elimination/gauss_elimination.py
@@ -0,0 +1,49 @@
+# Author  : Gu-hwan Bae
+# Summary : Get a row echolen form.
+
+import gula.solve as gsolve
+import numpy as np
+
+# Case I
+# All rows are linear independent. rank(M) = 3, null(M) = 0
+M = np.array([[1, 2, 3],
+              [1, 2, 4],
+              [3, 7, 4]])
+
+G = gsolve.gaussian.getRowEcholenForm(M)
+
+# Matrix G has three basis.
+print('>> Echolen form matrix =\n', G)
+
+# Case II
+# First and second row are linear dependent. rank(M) = 2, null(M) = 1
+M = np.array([[1, 2, 4],
+              [1, 2, 4],
+              [3, 7, 4]])
+
+G = gsolve.gaussian.getRowEcholenForm(M)
+
+# Matrix G has two basis.
+print('>> Echolen form matrix =\n', G)
+
+# Case III
+# Rows are standart generator of R3 field. rank(M) = 3, null(M) = 0
+M = np.array([[0, 0, 1],
+              [0, 1, 0],
+              [1, 0, 0]])
+
+G = gsolve.gaussian.getRowEcholenForm(M)
+
+# Matrix G has three basis.
+print('>> Echolen form matrix =\n', G)
+
+# Case IV
+# All rows are linear are linear dependent. rank(M) = 1, null(M) = 2
+M = np.array([[2, 2, 2],
+              [4, 4, 4],
+              [6, 6, 6]])
+
+G = gsolve.gaussian.getRowEcholenForm(M)
+
+# Matrix G has three basis.
+print('>> Echolen form matrix =\n', G)
